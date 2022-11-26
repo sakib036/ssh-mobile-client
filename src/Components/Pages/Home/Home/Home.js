3d50cc60da969f41600debc0f7a0c1d91a602369
Md. Nazmus Sakib <sakib036@gmail.com>
2022-11-25 17:21:19 -0800

    home-category

--- src/Components/Pages/Home/Home/Home.js
+++ src/Components/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Person from '../Person/Person';
+import PhoneCategory from '../PhoneCategory/PhoneCategory';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <PhoneCategory></PhoneCategory>
             <Person></Person>
         </div>
     );
---
+++ src/Components/Pages/Home/PhoneCategory/PhoneCategory.js
@@ -0,0 +1,142 @@
+import { useQuery } from '@tanstack/react-query';
+
+import React from 'react';
+import Loading from '../../../Common/Loading/Loading';
+
+// import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+// import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+// Import Swiper styles
+
+const PhoneCategory = () => {
+
+    const { data: mobiles = [], isLoading } = useQuery({
+        queryKey: ['mobiles'],
+        queryFn: () => fetch('http://localhost:5000/mobiles')
+            .then(res => res.json())
+    })
+    if (isLoading) {
+        return <Loading></Loading>
+    }
+    console.log(mobiles)
+
+    const samsungs = mobiles.filter(mobile => mobile.brandName === 'samsung');
+    console.log(samsungs);
+    const iphones = mobiles.filter(mobile => mobile.brandName === 'iphone');
+    console.log(iphones);
+    const others = mobiles.filter(mobile => mobile.brandName !== 'samsung' && mobile.brandName !== 'iphone');
+    console.log(others);
+
+
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+           <div>
+            <button className='text-3xl font-bold text-center my-6 btn btn-active btn-primary'>Samsung Brand</button>
+           <>
+                <Swiper
+                 
+
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper h-full w-full p-6"
+                >
+                    {
+                        samsungs.map(samsung => <SwiperSlide key={samsung._id}><div className="flex justify-center items-center shadow-xl border-2 border-blue-500 rounded-2xl">
+                            <figure><img className='w-96 h-52' src={samsung.picture} alt="Movie" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{(samsung.brandName).toUpperCase()}</h2>
+                                <p>{samsung.details}</p>
+                                <p> Price:{samsung.resalePrice}</p>
+
+                            </div>
+                        </div></SwiperSlide>)
+                    }
+                </Swiper>
+            </>
+           </div>
+
+            <div>
+                <button className='text-3xl font-bold text-center my-6 btn btn-active btn-secondary'>Iphone Brand</button>
+            <>              
+               <Swiper
+
+                   spaceBetween={30}
+                   centeredSlides={true}
+                   autoplay={{
+                       delay: 2500,
+                       disableOnInteraction: false,
+                   }}
+                   pagination={{
+                       clickable: true,
+                   }}
+                   navigation={true}
+                   modules={[Autoplay, Pagination, Navigation]}
+                   className="mySwiper h-full w-full p-6"
+               >
+                   {
+                       iphones.map(iphone => <SwiperSlide key={iphone._id}><div className="flex justify-center items-center shadow-xl border-2 border-blue-500 rounded-2xl">
+                           <figure><img className='w-96 h-52' src={iphone.picture} alt="Movie" /></figure>
+                           <div className="card-body">
+                               <h2 className="card-title">{(iphone.brandName).toUpperCase()}</h2>
+                               <p>{iphone.details}</p>
+                               <p> Price:{iphone.resalePrice}</p>
+
+                           </div>
+                       </div></SwiperSlide>)
+                   }
+               </Swiper>
+           </>
+            </div>
+
+            <div>
+                <button className='text-3xl font-bold text-center my-6 btn btn-active btn-info'>Others Brand</button>
+            <>
+              <Swiper
+
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    autoplay={{
+                        delay: 2500,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    navigation={true}
+                    modules={[Autoplay, Pagination, Navigation]}
+                    className="mySwiper h-full w-full p-6"
+                >
+                    {
+                        others.map(other => <SwiperSlide key={other._id}><div className="flex justify-center items-center shadow-xl border-2 border-blue-500 rounded-2xl">
+                            <figure><img className='w-96 h-52' src={other.picture} alt="Movie" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{(other.brandName).toUpperCase()}</h2>
+                                <p>{other.details}</p>
+                                <p> Price:{other.resalePrice}</p>
+
+                            </div>
+                        </div></SwiperSlide>)
+                    }
+                </Swiper>
+            </>
+            </div>
+        </div>
+    );
+};
+
+export default PhoneCategory;
